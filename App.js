8d688f1acc92dc9abba10a69be831753e7dbf89d
Flora <mazzavillanif@gmail.com>
2021-10-19 15:04:12 +1100

    added header, todoItem, addTodo

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, View, Alert, Keyboard} from 'react-native';
+import Header from './components/header'; 
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  };
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]; 
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Ciao Flora!</Text>
-      <StatusBar style="auto" />
+      {/*HEADER*/}
+      <Header/>
+      <View style={styles.content}>
+        {/*TO DO FORM*/}
+        <AddTodo sumbitHandler={submitHandler}/>
+        <View style={styles.list}>
+          {/*LIST*/}
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +51,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding:40,
+  },
+  list: {
+    marginTop:20,
   },
 });
